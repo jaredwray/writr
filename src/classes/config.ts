3be2da655e209cb3c0ea711528154114698b1787
Jared Wray <me@jaredwray.com>
2018-05-08 13:13:53 -0700

    adding in cache config

--- src/classes/config.ts
+++ src/classes/config.ts
@@ -2,4 +2,6 @@ export class Config {
     contentPath: string = './blog/content';
     postPath: string = './blog';
     templatePath: string = './blog/template';
+    cacheProvider: string = 'memory';
+
 }
