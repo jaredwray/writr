58b31cffa84154ad9424ac5c62d1e5203fdd7196
Jared Wray <me@jaredwray.com>
2018-05-14 11:26:09 -0700

    added in all the core crud functions for posts and tags

--- src/services/dataStore.ts
+++ src/services/dataStore.ts
@@ -28,10 +28,146 @@ export class DataStore {
             }
         } 
 
-        result = <Post>this.__cache.get(id);
+        result = <Post>this.__cache.get(cacheKey);
 
         return result;
     }
+
+    getPublishedPost(id:string) : Post | undefined {
+        let result = undefined;
+
+        let cacheKey = this.generatePostKey(id);
+
+        if(!this.__cache.has(cacheKey)) {
+            let post = this.getProvider().getPublishedPost(id);
+            
+            if(post) {
+                this.__cache.set(cacheKey, post!, undefined);
+            }
+        } 
+
+        result = <Post>this.__cache.get(cacheKey);
+
+        if(!result.isPostPublished()) {
+            result = undefined;
+        }
+
+        return result;
+    }
+
+    getPosts() : Array<Post> {
+        let result = new Array<Post>();
+
+        let id = 'get-posts';
+
+        let cacheKey = this.generatePostKey(id);
+
+        if(!this.__cache.has(cacheKey)) {
+            let posts = this.getProvider().getPosts();
+            
+            this.__cache.set(cacheKey, posts, undefined);
+        } 
+
+        result = <Post[]>this.__cache.get(cacheKey);
+
+        return result;
+
+    }
+
+    getPublishedPosts() : Array<Post> {
+        let result = new Array<Post>();
+
+        let id = 'get-published-posts';
+
+        let cacheKey = this.generatePostKey(id);
+
+        if(!this.__cache.has(cacheKey)) {
+            let posts = this.getProvider().getPublishedPosts();
+            
+            this.__cache.set(cacheKey, posts, undefined);
+        } 
+
+        result = <Post[]>this.__cache.get(cacheKey);
+
+        return result;
+
+    }
+
+    //tags
+    getTag(name:string) : Tag | undefined {
+        let result = undefined;
+
+        let cacheKey = this.generateTagKey(name);
+
+        if(!this.__cache.has(cacheKey)) {
+            let tag = this.getProvider().getTag(name);
+            
+            if(tag) {
+                this.__cache.set(cacheKey, tag!, undefined);
+            }
+        } 
+
+        result = <Tag>this.__cache.get(cacheKey);
+
+        return result;
+    }
+
+    getPublishedTag(name:string) : Tag | undefined {
+        let result = undefined;
+
+        let cacheKey = this.generateTagKey(name);
+
+        if(!this.__cache.has(cacheKey)) {
+            let tag = this.getProvider().getPublishedTag(name);
+            
+            if(tag) {
+                this.__cache.set(cacheKey, tag!, undefined);
+            }
+        } 
+
+        result = <Tag>this.__cache.get(cacheKey);
+
+        return result;
+    }
+
+    getTags() : Array<Tag> {
+        let result = new Array<Tag>();
+
+        let id = 'get-tags';
+
+        let cacheKey = this.generateTagKey(id);
+
+        if(!this.__cache.has(cacheKey)) {
+            let posts = this.getProvider().getTags();
+            
+            this.__cache.set(cacheKey, posts, undefined);
+        } 
+
+        result = <Tag[]>this.__cache.get(cacheKey);
+
+        return result;
+
+    }
+
+    getPublishedTags() : Array<Tag> {
+        let result = new Array<Tag>();
+
+        let id = 'get-published-tags';
+
+        let cacheKey = this.generateTagKey(id);
+
+        if(!this.__cache.has(cacheKey)) {
+            let posts = this.getProvider().getPublishedTags();
+            
+            this.__cache.set(cacheKey, posts, undefined);
+        } 
+
+        result = <Tag[]>this.__cache.get(cacheKey);
+
+        return result;
+
+    }
+    
 
     getProvider() : DataStoreProvider {
         let result: DataStoreProvider = new FileDataStore();
