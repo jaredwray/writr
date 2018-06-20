cd9b07421bc12f4ffa6d3799eb99114001f06cc8
Jared Wray <me@jaredwray.com>
2018-06-20 16:49:46 -0700

    format cleanup

--- src/data/dataCacheService.ts
+++ src/data/dataCacheService.ts
@@ -1,106 +1,103 @@
-import {Config} from '../config';
-import {Post} from '../post';
-import {Tag} from '../tag';
-import Keyv = require('keyv');
+import { Config } from "../config";
+import { Post } from "../post";
+import { Tag } from "../tag";
+import Keyv = require("keyv");
 
 export class DataCacheService {
+  private __postCache: Keyv;
+  private __tagCache: Keyv;
 
-    private __postCache: Keyv;
-    private __tagCache: Keyv;
+  constructor(config: Config) {
+    this.__postCache = new Keyv({ttl: config.cache.ttl, namespace: "data-post"});
+    this.__tagCache = new Keyv({ttl: config.cache.ttl, namespace: "data-tag"});
+  }
 
-    constructor(config:Config) {
-        this.__postCache = new Keyv({ttl: config.cache.ttl, namespace: 'data-post'});
-        this.__tagCache = new Keyv({ttl: config.cache.ttl, namespace: 'data-tag'});
+  //cache
+  async getPost(key: string): Promise<Post | undefined> {
+    key = this.formatName(key);
+
+    let result = await this.__postCache.get(key);
+
+    if (result) {
+      result = Post.create(result);
     }
 
+    return result;
+  }
 
-       //cache
-    async getPost(key:string) : Promise<Post | undefined> {
-        key = this.formatName(key);
+  async setPost(key: string, post: Post): Promise<boolean | undefined> {
+    return await this.__postCache.set(this.formatName(key), post);
+  }
 
-        let result = await this.__postCache.get(key);
+  async getPosts(key: string): Promise<Array<Post> | undefined> {
+    key = this.formatName(key);
 
-        if(result) {
-            result = Post.create(result);
-        }
+    let result = await this.__postCache.get(key);
 
-        return result;
+    if (result) {
+      let posts = new Array<Post>();
+
+      for (let i = 0; i < result.length; i++) {
+        posts.push(Post.create(result[i]));
+      }
+
+      result = posts;
     }
 
-    async setPost(key:string, post:Post) : Promise<boolean | undefined> {
-        return await this.__postCache.set(this.formatName(key), post);
+    return result;
+  }
+
+  async setPosts(
+    key: string,
+    posts: Array<Post>
+  ): Promise<boolean | undefined> {
+    return await this.__postCache.set(this.formatName(key), posts);
+  }
+
+  async getTag(key: string): Promise<Tag | undefined> {
+    key = this.formatName(key);
+
+    let result = await this.__tagCache.get(key);
+
+    if (result) {
+      result = Tag.create(result);
     }
 
-    async getPosts(key:string) : Promise<Array<Post> | undefined> {
-        key = this.formatName(key);
+    return result;
+  }
 
-        let result = await this.__postCache.get(key);
+  async setTag(key: string, tag: Tag): Promise<boolean | undefined> {
+    return await this.__tagCache.set(this.formatName(key), tag);
+  }
 
-        if(result) {
-            let posts = new Array<Post>();
+  async getTags(key: string): Promise<Array<Tag> | undefined> {
+    key = this.formatName(key);
 
-            for(let i=0;i<result.length;i++)
-            {
-                posts.push(Post.create(result[i]));
-            }
+    let result = await this.__tagCache.get(key);
 
-            result = posts;
-        }
+    if (result) {
+      let tags = new Array<Tag>();
 
-        return result;
+      for (let i = 0; i < result.length; i++) {
+        tags.push(Tag.create(result[i]));
+      }
+
+      result = tags;
     }
 
-    async setPosts(key:string, posts:Array<Post>) : Promise<boolean | undefined> {
-        return await this.__postCache.set(this.formatName(key), posts);
-    }
+    return result;
+  }
 
-    async getTag(key:string) : Promise<Tag | undefined> {
-        key = this.formatName(key);
+  async setTags(key: string, tags: Array<Tag>): Promise<boolean | undefined> {
+    return await this.__tagCache.set(this.formatName(key), tags);
+  }
 
-        let result = await this.__tagCache.get(key);
+  async clear(): Promise<void> {
+    await this.__postCache.clear();
+    await this.__tagCache.clear();
+  }
 
-        if(result) {
-            result = Tag.create(result);
-        }
-
-        return result;
-    }
-
-    async setTag(key:string, tag:Tag) : Promise<boolean | undefined> {
-        return await this.__tagCache.set(this.formatName(key), tag);
-    }
-
-    async getTags(key:string) : Promise<Array<Tag> | undefined> {
-        key = this.formatName(key);
-
-        let result = await this.__tagCache.get(key);
-
-        if(result) {
-            let tags = new Array<Tag>();
-
-            for(let i=0;i<result.length;i++)
-            {
-                tags.push(Tag.create(result[i]));
-            }
-
-            result = tags;
-        }
-
-        return result;
-    }
-
-    async setTags(key:string, tags:Array<Tag>) : Promise<boolean | undefined> {
-        return await this.__tagCache.set(this.formatName(key), tags);
-    }
-    
-    async clear(): Promise<void> {
-        await this.__postCache.clear();
-        await this.__tagCache.clear(); 
-    }
-
-    formatName(name:string) : string {
-        return name.toLowerCase().trim();
-    }
-
-
+  formatName(name: string): string {
+    return name.toLowerCase().trim();
+  }
 }
--- src/data/dataProviderInterface.ts
+++ src/data/dataProviderInterface.ts
@@ -1,15 +1,15 @@
-import {Config} from '../config';
-import {Post} from '../post';
-import {Tag} from '../tag';
+import { Config } from "../config";
+import { Post } from "../post";
+import { Tag } from "../tag";
 
 export interface DataProviderInterface {
-    getPost(id:string) : Promise<Post | undefined>;
-    getPublishedPost(id:string): Promise<Post | undefined>;
-    getPosts(): Promise<Array<Post>>;
-    getPublishedPosts(): Promise<Array<Post>>;
-    getTag(name:string) : Promise<Tag | undefined>;
-    getPublishedTag(name:string): Promise<Tag | undefined>;
-    getTags(): Promise<Array<Tag>>;
-    getPublishedTags(): Promise<Array<Tag>>;
-    init(config:Config): void;
+  getPost(id: string): Promise<Post | undefined>;
+  getPublishedPost(id: string): Promise<Post | undefined>;
+  getPosts(): Promise<Array<Post>>;
+  getPublishedPosts(): Promise<Array<Post>>;
+  getTag(name: string): Promise<Tag | undefined>;
+  getPublishedTag(name: string): Promise<Tag | undefined>;
+  getTags(): Promise<Array<Tag>>;
+  getPublishedTags(): Promise<Array<Tag>>;
+  init(config: Config): void;
 }
