87fa55169a33f814a30035643644709471901280
Jared Wray <me@jaredwray.com>
2019-08-16 16:45:15 -0700

    config updates
    broke the config into classes and added a parser for a data object.

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,65 @@
 export class Config {
-  data: any = {
-    type: "file",
-    contentPath: "./blog/content",
-    postPath: "./blog",
-    templatePath: "./blog/template"
-  };
-  cache = {
-    type: "memory",
-    connection: "",
-    ttl: 60000
-  };
+
+  data: ConfigData = new ConfigData();
+  cache: ConfigCache = new ConfigCache();
+
+  constructor(config: any = undefined) {
+
+    if(config) {
+      this.parse(config);
+    }
+  }
+
+  parse(obj:any) {
+    if(obj) {
+      if(obj.data) {
+        if(obj.data.type) {
+          this.data.type = obj.data.type;
+        }
+        if(obj.data.contentPath) {
+          this.data.contentPath = obj.data.contentPath;
+        }
+        if(obj.data.postPath) {
+          this.data.postPath = obj.data.postPath;
+        }
+        if(obj.data.templatePath) {
+          this.data.templatePath = obj.data.templatePath;
+        }
+      }
+      if(obj.cache) {
+        if(obj.cache.connection) {
+          this.cache.connection = obj.cache.connection;
+        }
+        if(obj.cache.ttl) {
+          this.cache.ttl = obj.cache.ttl;
+        }
+        if(obj.cache.type) {
+          this.cache.type = obj.cache.type;
+        }
+      }
+    }
+  }
+}
+
+export class ConfigData {
+
+  constructor() {
+
+  }
+
+  type: string = "file";
+  contentPath: string = "./blog/images";
+  postPath: string = "./blog";
+  templatePath: string = "./blog/template"
+}
+
+export class ConfigCache {
+
+  constructor() {
+
+  }
+
+  connection: string = "";
+  ttl: number = 6000;
+  type: string = "memory";
 }
---
+++ test/config.test.ts
@@ -0,0 +1,149 @@
+import { Tag } from '../src/tag';
+import { expect } from 'chai';
+import 'mocha';
+import { Config, ConfigCache, ConfigData } from '../src/config';
+
+describe('Config', () => {
+
+    let config = new Config();
+
+    beforeEach(async () => {
+
+        config = new Config();
+
+    });
+
+    it('config constructor should parse nothing', () => {
+
+        config = new Config(undefined);
+
+        expect(config.cache.type).to.equal("memory");
+    });
+
+    it('config constructor should parse data type', () => {
+
+        let obj = {
+            data: {
+                type: "blah"
+            }
+        }
+
+        config = new Config(obj);
+
+        expect(config.data.type).to.equal("blah");
+    });
+
+    it('config constructor should parse data contentPath', () => {
+
+        let obj = {
+            data: {
+                contentPath: "blah"
+            }
+        }
+
+        config = new Config(obj);
+
+        expect(config.data.contentPath).to.equal("blah");
+    });
+
+    it('config constructor should parse data postPath', () => {
+
+        let obj = {
+            data: {
+                postPath: "blah"
+            }
+        }
+
+        config = new Config(obj);
+
+        expect(config.data.postPath).to.equal("blah");
+    });
+
+    it('config constructor should parse data templatePath', () => {
+
+        let obj = {
+            data: {
+                templatePath: "blah"
+            }
+        }
+
+        config = new Config(obj);
+
+        expect(config.data.templatePath).to.equal("blah");
+    });
+
+    it('config constructor should parse cache connection', () => {
+
+        let obj = {
+            cache: {
+                connection: "blah"
+            }
+        }
+
+        config = new Config(obj);
+
+        expect(config.cache.connection).to.equal("blah");
+    });
+
+    it('config constructor should parse cache ttl', () => {
+
+        let obj = {
+            cache: {
+                ttl: 1111
+            }
+        }
+
+        config = new Config(obj);
+
+        expect(config.cache.ttl).to.equal(1111);
+    });
+
+    it('config constructor should parse cache type', () => {
+
+        let obj = {
+            cache: {
+                type: "blah"
+            }
+        }
+
+        config = new Config(obj);
+
+        expect(config.cache.type).to.equal("blah");
+    });
+
+    it('config should have default data type', () => {
+
+        expect(config.data.type).to.equal("file");
+    });
+
+    it('config should have default data contentPath', () => {
+
+        expect(config.data.contentPath).to.equal("./blog/images");
+    });
+
+    it('config should have default data postPath', () => {
+
+        expect(config.data.postPath).to.equal("./blog");
+    });
+
+    it('config should have default data templatePath', () => {
+
+        expect(config.data.templatePath).to.equal("./blog/template");
+    });
+
+    it('config should have default cache connection', () => {
+
+        expect(config.cache.connection).to.equal("");
+    });
+
+    it('config should have default cache ttl', () => {
+
+        expect(config.cache.ttl).to.equal(6000);
+    });
+
+    it('config should have default cache type', () => {
+
+        expect(config.cache.type).to.equal("memory");
+    });
+
+});
