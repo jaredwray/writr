454d95d2334a193595129722221391028804a40f
Jared Wray <me@jaredwray.com>
2019-10-08 21:15:56 -0700

    Create storageProviderInterface.ts

---
+++ src/storage/storageProviderInterface.ts
@@ -0,0 +1,7 @@
+export interface StorageProviderInterface {
+    get(path:string): Promise<string | undefined>;
+    set(path:string, data:string): Promise<boolean>;
+    delete(path:string): Promise<boolean>;
+    copy(src:string, dest:string): Promise<boolean>;
+    exists(path:string): Promise<boolean>;
+}
