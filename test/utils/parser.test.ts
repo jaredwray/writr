df3b88fd203ef2ba1f639729cb2f90b11b637f50
Christian Llontop <chrisllontop@icloud.com>
2022-03-01 22:32:31 -0500

    feat: add conditionals testing

--- test/utils/parser.test.ts
+++ test/utils/parser.test.ts
@@ -5,12 +5,20 @@ describe('parser', () => {
     it('should parse an html to markdown', () => {
 
         const parser = new Parser();
-
         const html = `<h1>This is a title</h1>`;
-
         const result = parser.htmlToMd(html);
 
         expect(result).toBe('# This is a title');
+
+    })
+
+    it('should return empty string if not html is provided', () => {
+
+        const parser = new Parser();
+        const html = ``;
+        const result = parser.htmlToMd(html);
+
+        expect(result).toBe('');
 
     })
 
