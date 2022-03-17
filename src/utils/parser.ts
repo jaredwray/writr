c25d0a384175f4247a481c800dc00e1cd1713158
Christian Llontop <chrisllontop@icloud.com>
2022-03-17 01:20:04 -0500

    style: add ident for files

--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -2,40 +2,40 @@ import {NodeHtmlMarkdown} from "node-html-markdown";
 
 export class Parser {
 
-    htmlToMd(html: string) {
-        if(!html) return '';
-        return new NodeHtmlMarkdown().translate(html);
-    }
+  htmlToMd(html: string) {
+    if (!html) return '';
+    return new NodeHtmlMarkdown().translate(html);
+  }
 
-    generateMdHeaders({title, slug, date, categories, tags}: Record<string, any>) {
+  generateMdHeaders({title, slug, date, categories, tags}: Record<string, any>) {
 
-        let header = '---\n';
-        if (title) header += `title: ${title}\n`;
-        if(slug) header += `url: ${slug}\n`;
-        if(date) header += `date: ${(new Date(date)).toISOString().slice(0, 10)}\n`;
-        if(categories) header += `categories: ${typeof categories === 'string' ? categories : categories.join(', ')}\n`;
-        if(tags) header += `tags: ${typeof tags === 'string' ? tags : tags.join(', ')}\n`;
-        header += '---'
+    let header = '---\n';
+    if (title) header += `title: ${title}\n`;
+    if (slug) header += `url: ${slug}\n`;
+    if (date) header += `date: ${(new Date(date)).toISOString().slice(0, 10)}\n`;
+    if (categories) header += `categories: ${typeof categories === 'string' ? categories : categories.join(', ')}\n`;
+    if (tags) header += `tags: ${typeof tags === 'string' ? tags : tags.join(', ')}\n`;
+    header += '---'
 
-        return header;
+    return header;
 
-    }
+  }
 
-    slugify(text: string) {
-        const from = "ãàáäâẽèéëêìíïîõòóöôùúüûñç·/_,:;"
-        const to = "aaaaaeeeeeiiiiooooouuuunc------"
+  slugify(text: string) {
+    const from = "ãàáäâẽèéëêìíïîõòóöôùúüûñç·/_,:;"
+    const to = "aaaaaeeeeeiiiiooooouuuunc------"
 
-        const newText = text.split('').map(
-          (letter, i) => letter.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i)))
+    const newText = text.split('').map(
+      (letter, i) => letter.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i)))
 
-        return newText
-          .toString()                     // Cast to string
-          .toLowerCase()                  // Convert the string to lowercase letters
-          .trim()                         // Remove whitespace from both sides of a string
-          .replace(/\s+/g, '-')           // Replace spaces with -
-          .replace(/&/g, '-y-')           // Replace & with 'and'
-          .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
-          .replace(/\-\-+/g, '-');        // Replace multiple - with single -
-    }
+    return newText
+      .toString()                     // Cast to string
+      .toLowerCase()                  // Convert the string to lowercase letters
+      .trim()                         // Remove whitespace from both sides of a string
+      .replace(/\s+/g, '-')           // Replace spaces with -
+      .replace(/&/g, '-y-')           // Replace & with 'and'
+      .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+      .replace(/\-\-+/g, '-');        // Replace multiple - with single -
+  }
 
 }
--- test/utils/parser.test.ts
+++ test/utils/parser.test.ts
@@ -2,47 +2,47 @@ import {Parser} from "../../src/utils/parser";
 
 describe('parser', () => {
 
-    const parser = new Parser();
+	const parser = new Parser();
 
-    it('should parse an html to markdown', () => {
+	it('should parse an html to markdown', () => {
 
-        const html = `<h1>This is a title</h1>`;
-        const result = parser.htmlToMd(html);
+		const html = `<h1>This is a title</h1>`;
+		const result = parser.htmlToMd(html);
 
-        expect(result).toBe('# This is a title');
+		expect(result).toBe('# This is a title');
 
-    })
+	})
 
-    it('should return empty string if not html is provided', () => {
+	it('should return empty string if not html is provided', () => {
 
-        const html = ``;
-        const result = parser.htmlToMd(html);
+		const html = ``;
+		const result = parser.htmlToMd(html);
 
-        expect(result).toBe('');
+		expect(result).toBe('');
 
-    })
+	})
 
-    it('should return slug based on a string', () => {
+	it('should return slug based on a string', () => {
 
-        const title = 'Hello, this is a title!';
-        const result = parser.slugify(title);
+		const title = 'Hello, this is a title!';
+		const result = parser.slugify(title);
 
-        expect(result).toBe('hello-this-is-a-title');
+		expect(result).toBe('hello-this-is-a-title');
 
-    })
+	})
 
-    it('should return the header of a markdown file', () => {
+	it('should return the header of a markdown file', () => {
 
-        const headers = {
-            title: 'Hello, this is a title!',
-            slug: 'hello-this-is-a-title',
-            date: '2020-01-01',
-            categories: 'category 1, category2',
-            tags: 'tag 1, tag 2'
-        };
-        const result = parser.generateMdHeaders(headers);
+		const headers = {
+			title: 'Hello, this is a title!',
+			slug: 'hello-this-is-a-title',
+			date: '2020-01-01',
+			categories: 'category 1, category2',
+			tags: 'tag 1, tag 2'
+		};
+		const result = parser.generateMdHeaders(headers);
 
-        const headersExpected = `---
+		const headersExpected = `---
 title: Hello, this is a title!
 url: hello-this-is-a-title
 date: 2020-01-01
@@ -50,8 +50,8 @@ categories: category 1, category2
 tags: tag 1, tag 2
 ---`
 
-        expect(result).toBe(headersExpected);
+		expect(result).toBe(headersExpected);
 
-    })
+	})
 
 });
