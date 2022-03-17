e9e15cfec8df5a7eff0dfdbcafa165950fd92f1a
Christian Llontop <chrisllontop@icloud.com>
2022-03-17 01:17:21 -0500

    feat: add test to header markdown and slug method

--- test/utils/parser.test.ts
+++ test/utils/parser.test.ts
@@ -2,9 +2,10 @@ import {Parser} from "../../src/utils/parser";
 
 describe('parser', () => {
 
+    const parser = new Parser();
+
     it('should parse an html to markdown', () => {
 
-        const parser = new Parser();
         const html = `<h1>This is a title</h1>`;
         const result = parser.htmlToMd(html);
 
@@ -14,11 +15,42 @@ describe('parser', () => {
 
     it('should return empty string if not html is provided', () => {
 
-        const parser = new Parser();
         const html = ``;
         const result = parser.htmlToMd(html);
 
         expect(result).toBe('');
+
+    })
+
+    it('should return slug based on a string', () => {
+
+        const title = 'Hello, this is a title!';
+        const result = parser.slugify(title);
+
+        expect(result).toBe('hello-this-is-a-title');
+
+    })
+
+    it('should return the header of a markdown file', () => {
+
+        const headers = {
+            title: 'Hello, this is a title!',
+            slug: 'hello-this-is-a-title',
+            date: '2020-01-01',
+            categories: 'category 1, category2',
+            tags: 'tag 1, tag 2'
+        };
+        const result = parser.generateMdHeaders(headers);
+
+        const headersExpected = `---
+title: Hello, this is a title!
+url: hello-this-is-a-title
+date: 2020-01-01
+categories: category 1, category2
+tags: tag 1, tag 2
+---`
+
+        expect(result).toBe(headersExpected);
 
     })
 
