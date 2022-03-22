b23082d22f41feae0168bba763edee90d5b96ad9
Christian Llontop <chrisllontop@icloud.com>
2022-03-22 13:32:31 -0500

    feat: add setup complete test

--- test/utils/setup.test.ts
+++ test/utils/setup.test.ts
@@ -1,5 +1,7 @@
+jest.mock("inquirer");
 import {Setup} from "../../src/utils/setup";
 import * as fs from "fs-extra";
+import * as inquirer from "inquirer";
 
 describe('setup', () => {
 
@@ -20,6 +22,44 @@ describe('setup', () => {
       expect(error.message).toBe('Directory already exists');
     } finally {
       fs.removeSync('./blog');
+    }
+  })
+
+
+  it('should create a md file from questions', async () => {
+    // @ts-ignore
+    inquirer.prompt.mockResolvedValue(Promise.resolve({
+      title: 'Test Title',
+      categories: 'category 1, category 2',
+      tags: 'tag 1, tag 2',
+      date: new Date().toLocaleDateString('en-CA'),
+    }));
+
+    await new Setup().new();
+
+    const mdFile = `---
+title: Test Title
+url: test-title
+date: 2022-03-22
+categories: category 1, category 2
+tags: tag 1, tag 2
+---`
+    expect(fs.readFileSync('./test-title.md', 'utf-8')).toBe(mdFile);
+    fs.removeSync('./test-title.md');
+  });
+
+  it('should throw an error when something fail creating a file', async () => {
+    // @ts-ignore
+    inquirer.prompt.mockResolvedValue(Promise.resolve({
+      categories: 'category 1, category 2',
+      tags: 'tag 1, tag 2',
+      date: new Date().toLocaleDateString('en-CA'),
+    }));
+
+    try{
+      await new Setup().new();
+    } catch (error: any){
+      expect(error.message).toBe('Error creating new file');
     }
   })
 
