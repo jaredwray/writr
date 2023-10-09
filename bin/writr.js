dfa565718f85e5dc381631608b0ac6826b2a872e
Jared Wray <me@jaredwray.com>
2023-10-09 09:57:12 -0700

    updating to node:process

--- bin/writr.js
+++ bin/writr.js
@@ -1,5 +1,6 @@
-#!/usr/bin/env ts-node --esm --inspect --experimental-specifier-resolution=node --es-module-specifier-resolution=node
+#!/usr/bin/env ts-node
 
+import {process} from 'node:process';
 import {Writr} from '../dist/index.js';
 
 const writr = new Writr();
