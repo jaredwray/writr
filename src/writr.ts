4ef79a08f79f39b80cddd128a1879a345124417c
Jared Wray <me@jaredwray.com>
2023-10-23 09:24:53 -0700

    adding in console support

--- src/writr.ts
+++ src/writr.ts
@@ -1,9 +1,11 @@
 import {WritrOptions} from './options.js';
 import {WritrHelpers} from './helpers.js';
+import {WritrConsole} from './console.js';
 
 export class Writr {
 	private _options: WritrOptions = new WritrOptions();
 	private readonly _helpers: WritrHelpers = new WritrHelpers();
+	private _console: WritrConsole = new WritrConsole();
 
 	constructor(options?: WritrOptions) {
 		if (options) {
@@ -21,5 +23,15 @@ export class Writr {
 
 	public get helpers(): WritrHelpers {
 		return this._helpers;
+	}
+
+	public parseCLI(process: NodeJS.Process): void {
+		//get the arguments
+		const args = process.argv.slice(2);
+
+		if(args.length === 0) {
+			this._console.printHelp();
+			return;
+		}
 	}
 }
