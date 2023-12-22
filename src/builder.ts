45f875f4fd9c491bbd704d985b7aac8c1c0c0a9d
Jared Wray <me@jaredwray.com>
2023-12-22 08:46:22 -0800

    adding build function

---
+++ src/builder.ts
@@ -0,0 +1,41 @@
+import {WritrOptions} from './options.js';
+import {type GithubData, Github} from './github.js';
+import { Ecto } from 'ecto';
+
+export type WritrData = {
+	github: GithubData;
+};
+
+export class WritrBuilder {
+	private readonly _options: WritrOptions = new WritrOptions();
+    private readonly _ecto: Ecto = new Ecto();
+	constructor(options?: WritrOptions) {
+		if (options) {
+			this._options = options;
+		}
+	}
+
+	public get options(): WritrOptions {
+		return this._options;
+	}
+
+	public async build(): Promise<void> {
+		// Validate the options
+
+		// get data from github
+
+		// get data of the site
+
+		// get the templates to use
+
+		// build the home page (index.html)
+
+		// build the releases page (/releases/index.html)
+
+		// build the rss feed (/rss.xml)
+
+        // build the sitemap (/sitemap.xml)
+
+        // build the robots.txt (/robots.txt)
+	}
+}
