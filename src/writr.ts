b0752e37c99a3e99348dada123404ce152f8dc52
Jared Wray <me@jaredwray.com>
2024-02-26 21:43:03 -0800

    adding in processing

--- src/writr.ts
+++ src/writr.ts
@@ -1,4 +1,4 @@
-import {unified} from 'unified';
+import {unified, Processor} from 'unified';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import rehypeSlug from 'rehype-slug';
@@ -17,17 +17,16 @@ type WritrOptions = {
 	gfm?: boolean; // Github flavor markdown (default: true)
 };
 
-class Writr {
-	private readonly _options: WritrOptions = {
-		openai: undefined,
-		emoji: true,
-		toc: true,
-		slug: true,
-		highlight: true,
-		gfm: true,
-	};
+type RenderOptions = {
+	emoji?: boolean; // Emoji support (default: true)
+	toc?: boolean; // Table of contents generation (default: true)
+	slug?: boolean; // Slug generation (default: true)
+	highlight?: boolean; // Code highlighting (default: true)
+	gfm?: boolean; // Github flavor markdown (default: true)
+};
 
-	private readonly processor = unified()
+class Writr {
+	public engine = unified()
 		.use(remarkParse)
 		.use(remarkGfm) // Use GitHub Flavored Markdown
 		.use(remarkToc, {heading: 'toc|table of contents'})
@@ -37,9 +36,20 @@ class Writr {
 		.use(rehypeHighlight) // Apply syntax highlighting
 		.use(rehypeStringify); // Stringify HTML
 
+	private readonly _options: WritrOptions = {
+		openai: undefined,
+		emoji: true,
+		toc: true,
+		slug: true,
+		highlight: true,
+		gfm: true,
+	};
+
 	constructor(options?: WritrOptions) {
 		if (options) {
 			this._options = {...this._options, ...options};
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+			this.engine = this.createProcessor(this._options);
 		}
 	}
 
@@ -47,15 +57,51 @@ class Writr {
 		return this._options;
 	}
 
-	async render(markdown: string): Promise<string> {
+	async render(markdown: string, options?: RenderOptions): Promise<string> {
 		try {
-			const file = await this.processor.process(markdown);
+			let {engine} = this;
+			if (options) {
+				// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+				engine = this.createProcessor(options);
+			}
+
+			const file = await engine.process(markdown);
 			return String(file);
 		} catch (error) {
 			throw new Error(`Failed to render markdown: ${(error as Error).message}`);
 		}
 	}
+
+	private createProcessor(options: RenderOptions | WritrOptions): any {
+		const processor = unified().use(remarkParse);
+
+		if (options.gfm ?? this._options.gfm) {
+			processor.use(remarkGfm);
+		}
+
+		if (options.toc ?? this._options.toc) {
+			processor.use(remarkToc, {heading: 'toc|table of contents'});
+		}
+
+		if (options.emoji ?? this._options.emoji) {
+			processor.use(remarkEmoji);
+		}
+
+		processor.use(remarkRehype);
+
+		if (options.slug ?? this._options.slug) {
+			processor.use(rehypeSlug);
+		}
+
+		if (options.highlight ?? this._options.highlight) {
+			processor.use(rehypeHighlight);
+		}
+
+		processor.use(rehypeStringify);
+
+		return processor;
+	}
 }
 
-export {Writr};
+export {Writr, type WritrOptions, type RenderOptions};
 
--- test/writr.test.ts
+++ test/writr.test.ts
@@ -7,4 +7,62 @@ describe('writr', () => {
 		const writr = new Writr();
 		expect(writr).toBeDefined();
 	});
+
+	it('should be able to set options', () => {
+		const options = {
+			emoji: false,
+			toc: false,
+			slug: false,
+			highlight: false,
+			gfm: false,
+		};
+		const writr = new Writr(options);
+		expect(writr.options).toBeDefined();
+		expect(writr.options.emoji).toEqual(false);
+		expect(writr.options.toc).toEqual(false);
+		expect(writr.options.slug).toEqual(false);
+		expect(writr.options.highlight).toEqual(false);
+		expect(writr.options.gfm).toEqual(false);
+	});
+
+	it('should be able to set options on emoji', () => {
+		const options = {
+			emoji: true,
+		};
+		const writr = new Writr(options);
+		expect(writr.options.emoji).toEqual(true);
+	});
+	it('should be able to set options on toc', () => {
+		const options = {
+			toc: true,
+		};
+		const writr = new Writr(options);
+		expect(writr.options.toc).toEqual(true);
+	});
+	it('should render a simple markdown example', async () => {
+		const writr = new Writr();
+		const result = await writr.render('# Hello World');
+		expect(result).toEqual('<h1 id="hello-world">Hello World</h1>');
+	});
+	it('should render a simple markdown example with options', async () => {
+		const writr = new Writr();
+		const options = {
+			slug: false,
+		};
+		const result = await writr.render('# Hello World', options);
+		expect(result).toEqual('<h1>Hello World</h1>');
+	});
+	it('should throw an error on bad plugin or parsing', async () => {
+		const writr = new Writr();
+		const customPlugin = () => {
+			throw new Error('Custom Plugin Error: Required configuration missing.');
+		};
+
+		writr.engine.use(customPlugin);
+		try {
+			await writr.render('# Hello World');
+		} catch (error) {
+			expect((error as Error).message).toEqual('Failed to render markdown: Custom Plugin Error: Required configuration missing.');
+		}
+	});
 });
