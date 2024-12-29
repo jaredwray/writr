15c383e46c1a9895ec3b0177bc0820421fd82323
Jared Wray <me@jaredwray.com>
2024-12-29 11:11:54 -0800

    adding in renderSync

--- test/writr-hooks.test.ts
+++ test/writr-hooks.test.ts
@@ -8,10 +8,18 @@ describe('Writr Render Hooks', async () => {
 	test('it should change the content before rendering', async () => {
 		const writr = new Writr('Hello, World!');
 		writr.onHook(WritrHooks.beforeRender, data => {
-			console.log(data);
 			data.body = 'Hello, Universe!';
 		});
 		const result = await writr.render();
 		expect(result).toBe('<p>Hello, Universe!</p>');
+	});
+
+	test('it should change the content before rendering sync', () => {
+		const writr = new Writr('Hello, World!');
+		writr.onHook(WritrHooks.beforeRender, data => {
+			data.body = 'Hello, Sync!';
+		});
+		const result = writr.renderSync();
+		expect(result).toBe('<p>Hello, Sync!</p>');
 	});
 });
