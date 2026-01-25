e0b44555ecaf99ccd8a0c5054d714494ab7d04cb
Jared Wray <me@jaredwray.com>
2026-01-25 10:42:37 -0800

    adding in minimal benchmark for straight performance

--- benchmark/benchmark-standard.ts
+++ benchmark/benchmark-standard.ts
@@ -1,8 +1,6 @@
 import fs from "node:fs";
 import { tinybenchPrinter } from "@monstermann/tinybench-pretty-printer";
 import { Bench } from "tinybench";
-import { marked } from "marked";
-import MarkdownIt from "markdown-it";
 import { Writr } from "../src/writr.js";
 import { benchmarkContents } from "./benchmark-contents.js";
 
@@ -13,40 +11,24 @@ const bench = new Bench({ name: `Writr v${pkg.version} Render`, iterations: 10_0
 const writr = new Writr({ renderOptions: { caching: false } });
 const writrCached = new Writr({ renderOptions: { caching: true } });
 
-// Configure marked with GFM support
-marked.setOptions({ gfm: true });
-
-// Initialize markdown-it with default settings
-const markdownIt = new MarkdownIt();
-
-bench.add("render (Async)", async () => {
+bench.add("Writr (Async)", async () => {
 	writr.content = benchmarkContents[Math.floor(Math.random() * benchmarkContents.length)];
 	await writr.render();
 });
 
-bench.add("render (Async) (cached)", async () => {
+bench.add("Writr (Async) (Caching)", async () => {
 	writr.content = benchmarkContents[Math.floor(Math.random() * benchmarkContents.length)];
 	await writrCached.render();
 });
 
-bench.add("render (Sync)", () => {
+bench.add("Writr (Sync)", () => {
 	writr.content = benchmarkContents[Math.floor(Math.random() * benchmarkContents.length)];
 	writr.renderSync();
 });
 
-bench.add("render (Sync) (cached)", () => {
+bench.add("Writr (Sync) (Caching)", () => {
 	writr.content = benchmarkContents[Math.floor(Math.random() * benchmarkContents.length)];
 	writrCached.renderSync();
-});
-
-bench.add("marked", () => {
-	const content = benchmarkContents[Math.floor(Math.random() * benchmarkContents.length)];
-	marked.parse(content);
-});
-
-bench.add("markdown-it", () => {
-	const content = benchmarkContents[Math.floor(Math.random() * benchmarkContents.length)];
-	markdownIt.render(content);
 });
 
 await bench.run();
